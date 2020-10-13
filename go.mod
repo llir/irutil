af3d8eb90900a7fe409aca3c97bd31325ee15e24
Robin Eklind <rnd0x00@gmail.com>
2020-10-13 14:08:34 +0200

    irutil: update to use latest llir/llvm release
    This issue was identified by @dannypsnl

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/llir/irutil
 
 go 1.12
 
-require github.com/llir/llvm v0.3.0-pre7.0.20191220191840-fc6d0e8d9bc6
+require github.com/llir/llvm v0.3.2
--- walk.go
+++ walk.go
@@ -408,10 +408,6 @@ func walk(root interface{}, visit func(n interface{}) bool, visited map[interfac
 		walk(*root, visit, visited)
 	case *value.Named:
 		walk(*root, visit, visited)
-	case *ir.ExceptionScope:
-		walk(*root, visit, visited)
-	case *ir.UnwindTarget:
-		walk(*root, visit, visited)
 	// Metadata.
 	case *metadata.Node:
 		walk(*root, visit, visited)
@@ -516,10 +512,6 @@ func walk(root interface{}, visit func(n interface{}) bool, visited map[interfac
 		walkValue(root, visit, visited)
 	case value.Named:
 		walkValueNamed(root, visit, visited)
-	case ir.ExceptionScope:
-		walkValue(root, visit, visited)
-	case ir.UnwindTarget:
-		walkUnwindTarget(root, visit, visited)
 	case metadata.SpecializedNode:
 		walkSpecializedMetadataNode(root, visit, visited)
 	default:
@@ -878,8 +870,8 @@ func walkInst(root ir.Instruction, visit func(n interface{}) bool, visited map[i
 		}
 	case *ir.InstSelect:
 		walk(&root.Cond, visit, visited)
-		walk(&root.X, visit, visited)
-		walk(&root.Y, visit, visited)
+		walk(&root.ValueTrue, visit, visited)
+		walk(&root.ValueFalse, visit, visited)
 	case *ir.InstCall:
 		walk(&root.Callee, visit, visited)
 		for i := range root.Args {
@@ -892,12 +884,12 @@ func walkInst(root ir.Instruction, visit func(n interface{}) bool, visited map[i
 			walk(&root.Clauses[i], visit, visited)
 		}
 	case *ir.InstCatchPad:
-		walk(&root.Scope, visit, visited)
+		walk(&root.CatchSwitch, visit, visited)
 		for i := range root.Args {
 			walk(&root.Args[i], visit, visited)
 		}
 	case *ir.InstCleanupPad:
-		walk(&root.Scope, visit, visited)
+		walk(&root.ParentPad, visit, visited)
 		for i := range root.Args {
 			walk(&root.Args[i], visit, visited)
 		}
@@ -938,25 +930,29 @@ func walkTerm(root ir.Terminator, visit func(n interface{}) bool, visited map[in
 		for i := range root.Args {
 			walk(&root.Args[i], visit, visited)
 		}
-		walk(&root.Normal, visit, visited)
-		walk(&root.Exception, visit, visited)
+		walk(&root.NormalRetTarget, visit, visited)
+		walk(&root.ExceptionRetTarget, visit, visited)
 		for i := range root.OperandBundles {
 			walk(&root.OperandBundles[i], visit, visited)
 		}
 	case *ir.TermResume:
 		walk(&root.X, visit, visited)
 	case *ir.TermCatchSwitch:
-		walk(&root.Scope, visit, visited)
+		walk(&root.ParentPad, visit, visited)
 		for i := range root.Handlers {
 			walk(&root.Handlers[i], visit, visited)
 		}
-		walk(&root.UnwindTarget, visit, visited)
+		if root.DefaultUnwindTarget != nil {
+			walk(&root.DefaultUnwindTarget, visit, visited)
+		}
 	case *ir.TermCatchRet:
-		walk(&root.From, visit, visited)
-		walk(&root.To, visit, visited)
+		walk(&root.CatchPad, visit, visited)
+		walk(&root.Target, visit, visited)
 	case *ir.TermCleanupRet:
-		walk(&root.From, visit, visited)
-		walk(&root.UnwindTarget, visit, visited)
+		walk(&root.CleanupPad, visit, visited)
+		if root.UnwindTarget != nil {
+			walk(&root.UnwindTarget, visit, visited)
+		}
 	case *ir.TermUnreachable:
 		// nothing to do
 	default:
@@ -997,20 +993,6 @@ func walkValueNamed(root value.Named, visit func(n interface{}) bool, visited ma
 		walk(root, visit, visited)
 	case *ir.TermCatchSwitch:
 		walk(root, visit, visited)
-	default:
-		panic(fmt.Errorf("support for LLVM IR AST node type %T not yet implemented", root))
-	}
-}
-
-// walkUnwindTarget walks the LLVM IR AST in depth-first order; invoking visit
-// recursively for each non-nil child of root. If visit returns false, the walk
-// is terminated. Visited tracks visited nodes.
-func walkUnwindTarget(root ir.UnwindTarget, visit func(n interface{}) bool, visited map[interface{}]bool) {
-	switch root := root.(type) {
-	case *ir.Block:
-		walk(root, visit, visited)
-	case ir.UnwindToCaller:
-		// nothing to do
 	default:
 		panic(fmt.Errorf("support for LLVM IR AST node type %T not yet implemented", root))
 	}
