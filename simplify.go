d8043a25110cf9589e62aed2871df598fe624938
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-31 10:20:58 +0800

    simplify: don't affect origin instance

--- simplify.go
+++ simplify.go
@@ -14,58 +14,74 @@ func Simplify(c constant.Constant) constant.Constant {
 		x, ok := Simplify(c.X).(*constant.Int)
 		y, ok2 := Simplify(c.Y).(*constant.Int)
 		if ok && ok2 {
-			x.X = (&big.Int{}).Add(x.X, y.X)
+			z := constant.NewInt(x.Typ, 0)
+			z.X = (&big.Int{}).Add(x.X, y.X)
+			return z
 		}
-		return x
+		return c
 	case *constant.ExprSub:
 		x, ok := Simplify(c.X).(*constant.Int)
 		y, ok2 := Simplify(c.Y).(*constant.Int)
 		if ok && ok2 {
-			x.X = (&big.Int{}).Sub(x.X, y.X)
+			z := constant.NewInt(x.Typ, 0)
+			z.X = (&big.Int{}).Sub(x.X, y.X)
+			return z
 		}
-		return x
+		return c
 	case *constant.ExprMul:
 		x, ok := Simplify(c.X).(*constant.Int)
 		y, ok2 := Simplify(c.Y).(*constant.Int)
 		if ok && ok2 {
-			x.X = (&big.Int{}).Mul(x.X, y.X)
+			z := constant.NewInt(x.Typ, 0)
+			z.X = (&big.Int{}).Mul(x.X, y.X)
+			return z
 		}
-		return x
+		return c
 	case *constant.ExprSDiv:
 		x, ok := Simplify(c.X).(*constant.Int)
 		y, ok2 := Simplify(c.Y).(*constant.Int)
 		if ok && ok2 {
-			x.X = (&big.Int{}).Div(x.X, y.X)
+			z := constant.NewInt(x.Typ, 0)
+			z.X = (&big.Int{}).Div(x.X, y.X)
+			return z
 		}
-		return x
+		return c
 	case *constant.ExprUDiv:
 		x, ok := Simplify(c.X).(*constant.Int)
 		y, ok2 := Simplify(c.Y).(*constant.Int)
 		if ok && ok2 {
-			x.X = (&big.Int{}).Div(x.X, y.X)
+			z := constant.NewInt(x.Typ, 0)
+			z.X = (&big.Int{}).Div(x.X, y.X)
+			return z
 		}
-		return x
+		return c
 	case *constant.ExprFAdd:
 		x, ok := Simplify(c.X).(*constant.Float)
 		y, ok2 := Simplify(c.Y).(*constant.Float)
 		if ok && ok2 {
-			x.X = (&big.Float{}).Add(x.X, y.X)
+			z := constant.NewFloat(x.Typ, 0)
+			z.X = (&big.Float{}).Add(x.X, y.X)
+			return z
 		}
-		return x
+		return c
 	case *constant.ExprFSub:
 		x, ok := Simplify(c.X).(*constant.Float)
 		y, ok2 := Simplify(c.Y).(*constant.Float)
 		if ok && ok2 {
-			x.X = (&big.Float{}).Sub(x.X, y.X)
+			z := constant.NewFloat(x.Typ, 0)
+			z.X = (&big.Float{}).Sub(x.X, y.X)
+			return z
 		}
-		return x
+		return c
 	case *constant.ExprFMul:
 		x, ok := Simplify(c.X).(*constant.Float)
 		y, ok2 := Simplify(c.Y).(*constant.Float)
 		if ok && ok2 {
-			x.X = (&big.Float{}).Mul(x.X, y.X)
+			z := constant.NewFloat(x.Typ, 0)
+			z.X = (&big.Float{}).Mul(x.X, y.X)
+			return z
 		}
-		return x
+		return c
 	default:
 		return c
 	}
